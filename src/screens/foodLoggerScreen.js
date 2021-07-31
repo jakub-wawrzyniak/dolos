c9dfe3322af70cdec48e17c4502bfb25807d56b8
wojtek-kowalczyk <wojtekkowalczyk01@gmail.com>
2021-07-31 18:30:30 +0200

    implemented a progress circle component
    a usable component. fiddled with Victory Native Lib for data visualization. Their docs are pretty vague so I don't 100% understand the code but it works, you can import and pass props (ctrl + space in vsc to se what props are available, but for more details on what props mean you have to look intp progressCircle.js - source code for this component) to customize.

--- src/screens/foodLoggerScreen.js
+++ src/screens/foodLoggerScreen.js
@@ -1,48 +1,40 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, useWindowDimensions} from 'react-native';
-import Svg from 'react-native-svg';
-import {VictoryPie, VictoryLabel, VictoryAnimation} from 'victory-native';
-import styles from '../global/styles';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useWindowDimensions,
+  Button,
+} from 'react-native';
+import ProgressCircle from '../components/progressCirle';
 
 import globalStyles from '../global/styles';
 
 function FoodLoggerScreen() {
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
-  const [percent, setPercent] = useState(25);
-
-  const data = [
-    {x: 1, y: percent},
-    {x: 2, y: 100 - percent},
-  ];
-
-  // todo - continue progress circle
-  // https://formidable.com/open-source/victory/docs/victory-label#active
-
+  const [val, setval] = useState(46);
   return (
     <View style={globalStyles.container}>
-      <Svg
-        height={150}
-        width={windowWidth - globalStyles.container.padding * 2}
-        style={{backgroundColor: 'dodgerblue'}}>
-        <VictoryPie
-          data={data}
-          height={150}
-          width={windowWidth - globalStyles.container.padding * 2}
+      <View style={{alignSelf: 'center'}}>
+        <ProgressCircle
+          progress={val}
+          size={150}
+          fontSize={28}
           padding={15}
-          innerRadius={50}
-          labels={() => null}
+          animateDuration={800}
+          thickness={10}
         />
-        <VictoryLabel
-          x={(windowWidth - globalStyles.container.padding * 2) / 2}
-          y={75}
-          backgroundStyle={{backgroundColor: 'red'}}
-          textAnchor="middle"
-          verticalAnchor="middle"
-          text="5%"
-          style={{fontSize: 45}}
-        />
-      </Svg>
+      </View>
+      <Button
+        title="add %"
+        // cap at 100 setPercent((percent)=>{ logic - returns value })
+        onPress={() =>
+          setval(val => {
+            return val + 15 <= 100 ? val + 15 : 100;
+          })
+        }
+        color="red"
+      />
+      <Button title="RESET" onPress={() => setval(0)} color="red" />
     </View>
   );
 }
