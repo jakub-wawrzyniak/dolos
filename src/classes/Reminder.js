14eb3eae6a2dafd0e87e8c59f2332cbc0b280c7a
wojtek-kowalczyk <wojtekkowalczyk01@gmail.com>
2021-08-03 16:43:35 +0200

    Added functionality to Reminder.js generating id, removing all notifications and most importantly, generating a date string

--- src/classes/Reminder.js
+++ src/classes/Reminder.js
@@ -1,6 +1,7 @@
 import PushNotification from 'react-native-push-notification';
 
 export const createReminder = ({
+  id, // we have to give custom ID so that we can track it.
   title,
   message,
   date,
@@ -8,14 +9,20 @@ export const createReminder = ({
   userData,
 }) => {
   PushNotification.localNotificationSchedule({
+    id: id,
     title: 'Reminder!' || title,
     message: message,
     date: date,
-    allowWhileIdle: true,
-    channelId: 'main',
     repeatTime: 1 || repeatTime,
     userInfo: {} || userData,
+    allowWhileIdle: true,
+    channelId: 'main',
   });
+};
+
+export const generateID = () => {
+  // Might want to change that in the future.
+  return ((Math.random() * 4294967296) / 2 - 1) >>> 0;
 };
 
 export const editReminder = (id, {title, message, date, userData}) => {
@@ -34,10 +41,36 @@ export const editReminder = (id, {title, message, date, userData}) => {
   PushNotification.cancelLocalNotifications({id: id});
   createReminder({title, message, date, userData});
 };
-
+export const cancelAllNotifications = () => {
+  console.log('Cancelling...');
+  PushNotification.cancelAllLocalNotifications();
+};
 export const getAllReminders = () =>
   PushNotification.getScheduledLocalNotifications(all => {
     all.forEach(elem => {
       console.log(elem);
     });
   });
+
+// this seemed async (when doing multiple logs) and thus this horror was born.
+export const getNotificationDateFormat = async function (id) {
+  let string = 'dupa';
+  await new Promise((resolve, reject) => {
+    PushNotification.getScheduledLocalNotifications(notifications => {
+      notifications.every(notification => {
+        if (notification.id == id) {
+          const d = notification.date;
+          string = `0${d.getDay() + 1}-${
+            d.getMonth() < 10 ? 0 : ''
+          }${d.getMonth()}-${d.getFullYear()} at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+          return false;
+        }
+        return true;
+      });
+      resolve(string);
+    });
+  }).then(returnval => {
+    string = returnval;
+  });
+  return string;
+};
